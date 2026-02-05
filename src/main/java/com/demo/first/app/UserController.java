a48258e3b32680651e0c02197d979b341fb20ede
shivanshu-goswami <goswamishivanshu47@gmail.com>
2026-02-05 17:07:38 +0530

    REST API With Postman Testing

--- src/main/java/com/demo/first/app/UserController.java
+++ src/main/java/com/demo/first/app/UserController.java
@@ -1,8 +1,12 @@
 package com.demo.first.app;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -11,17 +15,35 @@ public class UserController {
     private Map<Integer,User> userDb = new HashMap<Integer,User>();
 
     @PostMapping
-    public String createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user){
         System.out.println(user.getEmail());
         userDb.putIfAbsent(user.getId(),user);
-        return "User created";
+        //return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        return new ResponseEntity<>(user,HttpStatus.CREATED);
     }
 
     @PutMapping
-    public String updateUser(@RequestBody User user) {
-        if (userDb.containsKey(user.getId())) {
-            userDb.put(user.getId(), user);
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        if (!userDb.containsKey(user.getId())) {
+           // return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        return "Update successful";
+        //return ResponseEntity.status(HttpStatus.OK).body(user);
+        return ResponseEntity.ok(user);
+    }
+    // /user/1   /user/2  /user/3
+    @DeleteMapping("/{id}")
+        public ResponseEntity<String> deleteUser(@PathVariable int id) {
+           if(!userDb.containsKey(id)) {
+               return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+           }
+           userDb.remove(id);
+           //return ResponseEntity.noContent().build();
+           //no content will give status code 204 which is used in this place
+           return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+    @GetMapping
+    public List<User> getUsers() {
+        return new ArrayList<User>(userDb.values());
     }
 }
